9e883cff24439b20b0a62b003227907c43aee55b
deadalnix <deadalnix@gmail.com>
2011-10-29 16:47:57 +0200

    Add all.D files to include whole modules

---
+++ include/dsfml/audio/all.d
@@ -0,0 +1,11 @@
+module dsfml.audio.all;
+
+public import dsfml.audio.listener;
+public import dsfml.audio.music;
+public import dsfml.audio.sound;
+public import dsfml.audio.soundbuffer;
+public import dsfml.audio.soundbufferrecorder;
+public import dsfml.audio.soundrecorder;
+public import dsfml.audio.soundsource;
+public import dsfml.audio.soundstream;
+
---
+++ include/dsfml/system/all.d
@@ -0,0 +1,8 @@
+module dsfml.system.all;
+
+public import dsfml.system.clock;
+public import dsfml.system.inputstream;
+public import dsfml.system.lock;
+public import dsfml.system.mutex;
+public import dsfml.system.sleep;
+
