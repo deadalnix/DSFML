7a28d17d08112a7945462a81bb035ec6cb4a5889
deadalnix <deadalnix@gmail.com>
2011-11-02 22:01:23 +0100

    SSE optmized color

--- include/dsfml/graphics/color.d
+++ include/dsfml/graphics/color.d
@@ -1,7 +1,5 @@
 module dsfml.graphics.color;
 
-import csfml.graphics.color;
-
 import dsfml.system.utils;
 
 immutable Color black	= Color(0, 0, 0);
@@ -33,23 +31,23 @@ struct Color {
 	public Color opAdd(const Color other) const {
 		static if(sseAvailable) {
 			auto thisptr	= &this;
-			auto ret		= Color();
+			Color ret		= void;
 			
 			version(D_InlineAsm_X86) {
 				asm {
 					mov EAX, thisptr;
-					movss XMM0, [EAX];
-					movss XMM1, other;
-					paddusb XMM0, XMM1;
-					movss ret, XMM0;
+					movss XMM0, [EAX];	// XMM0 is this
+					movss XMM1, other;	// XMM1 is other
+					paddusb XMM0, XMM1;	// Saturated addition
+					movss ret, XMM0;	// Save result to ret
 				}
 			} else version(D_InlineAsm_X86_64) {
 				asm {
 					mov RAX, thisptr;
-					movss XMM0, [RAX];
-					movss XMM1, other;
-					paddusb XMM0, XMM1;
-					movss ret, XMM0;
+					movss XMM0, [RAX];	// XMM0 is this
+					movss XMM1, other;	// XMM1 is other
+					paddusb XMM0, XMM1;	// Saturated addition
+					movss ret, XMM0;	// Save result to ret
 				}
 			}
 			
@@ -66,18 +64,18 @@ struct Color {
 			version(D_InlineAsm_X86) {
 				asm {
 					mov EAX, thisptr;
-					movss XMM0, [EAX];
-					movss XMM1, other;
-					paddusb XMM0, XMM1;
-					movss [EAX], XMM0;
+					movss XMM0, [EAX];	// XMM0 is this
+					movss XMM1, other;	// XMM1 is other
+					paddusb XMM0, XMM1;	// Saturated addition
+					movss [EAX], XMM0;	// Save result to this
 				}
 			} else version(D_InlineAsm_X86_64) {
 				asm {
 					mov RAX, thisptr;
-					movss XMM0, [RAX];
-					movss XMM1, other;
-					paddusb XMM0, XMM1;
-					movss [RAX], XMM0;
+					movss XMM0, [RAX];	// XMM0 is this
+					movss XMM1, other;	// XMM1 is other
+					paddusb XMM0, XMM1;	// Saturated addition
+					movss [RAX], XMM0;	// Save result to this
 				}
 			}
 		} else {
@@ -93,47 +91,87 @@ struct Color {
 	// TODO: consider how to do this with MMX/SSE instructions
 	// PUNPCKLBW can probably help here
 	public Color opMul(const Color other) const {
-		static if(sseAvailable && false) {
+		static if(sseAvailable) {
 			auto thisptr	= &this;
-			auto ret		= Color();
+			ushort[4] ret;
 			
 			version(D_InlineAsm_X86) {
-				static assert(false, "fail badly !");
+				asm {
+					mov EAX, thisptr;
+					movss XMM0, [EAX];		// XMM0 is this
+					movss XMM1, other;		// XMM1 is other
+					xorps XMM2, XMM2;		// XMM2 is 0
+					punpcklbw XMM0, XMM2;	// XMM0 contains this packed as ushort[4]
+					punpcklbw XMM1, XMM2;	// XMM1 other this packed as ushort[4]
+					pmullw XMM0, XMM1;		// XMM0 contains the multiplication of the two colors.
+					movq ret, XMM0;
+				}
 			} else version(D_InlineAsm_X86_64) {
 				asm {
 					mov RAX, thisptr;
-					movss XMM0, [RAX];
-					movss XMM1, other;
-					xorps XMM2, XMM2;		// Set XMM2 to 0
-					packsswb XMM0, XMM2;	// XMM0 contains this packed as ushort[4]
-					packsswb XMM1, XMM2;	// XMM1 other this packed as ushort[4]
+					movss XMM0, [RAX];		// XMM0 is this
+					movss XMM1, other;		// XMM1 is other
+					xorps XMM2, XMM2;		// XMM2 is 0
+					punpcklbw XMM0, XMM2;	// XMM0 contains this packed as ushort[4]
+					punpcklbw XMM1, XMM2;	// XMM1 other this packed as ushort[4]
 					pmullw XMM0, XMM1;		// XMM0 contains the multiplication of the two colors.
-					andnps XMM2, XMM2;		// XMM2 to [255, 255, 255, 255]
-					
-					// What to do now ?
+					movq ret, XMM0;
 				}
 			}
 			
-			return ret;
+			return Color(cast(ubyte) (ret[0] / 255), cast(ubyte) (ret[1] / 255), cast(ubyte) (ret[2] / 255), cast(ubyte) (ret[3] / 255));
 		} else {
 			return Color(modulate(r, other.r), modulate(g, other.g), modulate(b, other.b), modulate(a, other.a));
 		}
 	}
 	
 	public ref Color opMulAssign(const Color other) {
-		r = modulate(r, other.r);
-		g = modulate(g, other.g);
-		b = modulate(b, other.b);
-		a = modulate(a, other.a);
+		static if(sseAvailable) {
+			auto thisptr	= &this;
+			ushort[4] ret;
+			
+			version(D_InlineAsm_X86) {
+				asm {
+					mov EAX, thisptr;
+					movss XMM0, [EAX];		// XMM0 is this
+					movss XMM1, other;		// XMM1 is other
+					xorps XMM2, XMM2;		// XMM2 is 0
+					punpcklbw XMM0, XMM2;	// XMM0 contains this packed as ushort[4]
+					punpcklbw XMM1, XMM2;	// XMM1 other this packed as ushort[4]
+					pmullw XMM0, XMM1;		// XMM0 contains the multiplication of the two colors.
+					movq ret, XMM0;
+				}
+			} else version(D_InlineAsm_X86_64) {
+				asm {
+					mov RAX, thisptr;
+					movss XMM0, [RAX];		// XMM0 is this
+					movss XMM1, other;		// XMM1 is other
+					xorps XMM2, XMM2;		// XMM2 is 0
+					punpcklbw XMM0, XMM2;	// XMM0 contains this packed as ushort[4]
+					punpcklbw XMM1, XMM2;	// XMM1 other this packed as ushort[4]
+					pmullw XMM0, XMM1;		// XMM0 contains the multiplication of the two colors.
+					movq ret, XMM0;
+				}
+			}
+			
+			// Sadly, no SIMD instruction for integer division.
+			r = cast(ubyte) (ret[0] / 255);
+			g = cast(ubyte) (ret[1] / 255);
+			b = cast(ubyte) (ret[2] / 255);
+			a = cast(ubyte) (ret[3] / 255);
+		} else {
+			r = modulate(r, other.r);
+			g = modulate(g, other.g);
+			b = modulate(b, other.b);
+			a = modulate(a, other.a);
+		}
 		
 		return this;
 	}
 }
 
 unittest {
-	import std.stdio;
-	
-	// TODO: add unitests
+	// TODO: add moar unitests
 	Color c = black;
 	assert((c += white) == white);
 	c= magenta;
@@ -142,6 +180,8 @@ unittest {
 	assert((black + white) == white);
 	assert((magenta + cyan) == white);
 	
-	writeln("OK");
+	assert((magenta * cyan) == blue);
+	c = magenta;
+	assert((c *= cyan) == blue);
 }
 
