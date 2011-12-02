7c57759bc26e2b6373754b896cf8ae20a5b7e679
deadalnix <deadalnix@gmail.com>
2011-12-03 00:40:42 +0100

    all all.d modules

---
+++ src/dsfml/graphics/all.d
@@ -0,0 +1,10 @@
+module dsfml.graphics.all;
+
+public import dsfml.graphics.color;
+public import dsfml.graphics.font;
+public import dsfml.graphics.glyph;
+public import dsfml.graphics.image;
+public import dsfml.graphics.rect;
+public import dsfml.graphics.renderwindow;
+public import dsfml.graphics.texture;
+
